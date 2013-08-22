87c6dd953be371efc37c77c8e0090bbb8e650a82
Kolie <kolies@gmail.com>
2013-08-22 12:34:33 +0200

    Refactored users

--- SingletonTheory.Web.AuthApp/app/js/filters/Interpolate.js
+++ SingletonTheory.Web.AuthApp/app/js/filters/Interpolate.js
@@ -2,7 +2,7 @@
 
 angular.module('myApp.filters', []).
   filter('interpolate', ['version', function (version) {
-  	return function (text) {
-  		return String(text).replace(/\%VERSION\%/mg, version);
-  	}
+	  return function(text) {
+		  return String(text).replace(/\%VERSION\%/mg, version);
+	  };
   }]);
