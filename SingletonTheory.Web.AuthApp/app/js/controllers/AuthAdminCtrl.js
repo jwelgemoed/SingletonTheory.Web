fd177fcd29db5c64d089157ed75a7585aa79defb
AlmeroSteyn <almero.steyn@gmail.com>
2013-09-13 14:47:38 +0200

    Basic use of auth services in front end

--- SingletonTheory.Web.AuthApp/app/js/controllers/AuthAdminCtrl.js
+++ SingletonTheory.Web.AuthApp/app/js/controllers/AuthAdminCtrl.js
@@ -1,12 +1,50 @@
 ﻿'use strict';
 
-userApplicationModule.controller('AuthAdminCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
-	
-	$scope.element = "Role";
-	
-	$scope.selectElement = function (elementName) {
-		$scope.element = elementName;
-	};
+userApplicationModule.controller('AuthAdminCtrl', ['$rootScope', '$scope', 'AuthAdminRoleResource', 'AuthAdminGroupLvl2Resource',
+	'AuthAdminGroupLvl1Resource', 'AuthAdminPermissionResource', function ($rootScope, $scope, AuthAdminRoleResource, AuthAdminGroupLvl2Resource, AuthAdminGroupLvl1Resource,
+	AuthAdminPermissionResource) {
 
-}]);
+		$scope.element = "Role";
+
+		$scope.error = '';
+
+		$scope.elementDictionary = [];
+
+		$scope.selectElement = function (elementName) {
+			$scope.element = elementName;
+			getElementData();
+		};
+		
+		function getElementData () {
+			switch ($scope.element) {
+				case 'Role':
+					AuthAdminRoleResource.query({}, function (result) {
+						$scope.elementDictionary = result;
+					}, function (err) { $scope.error = err; }
+					);
+					break;
+				case 'Permission Group Level 1':
+					AuthAdminGroupLvl1Resource.query({}, function (result) {
+						$scope.elementDictionary = result;
+					}, function (err) { $scope.error = err; }
+					);
+					break;
+				case 'Permission Group Level 2':
+					AuthAdminGroupLvl2Resource.query({}, function (result) {
+						$scope.elementDictionary = result;
+					}, function (err) { $scope.error = err; }
+					);
+					break;
+				case 'Permission':
+					AuthAdminPermissionResource.query({}, function (result) {
+						$scope.elementDictionary = result;
+					}, function (err) { $scope.error = err; }
+					);
+					break;
+			}
+		};
+
+
+
+	}]);
 
