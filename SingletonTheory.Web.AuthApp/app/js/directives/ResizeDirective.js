f1927a63d8fbf8418aee7cf27de0ce32dba5e952
AlmeroSteyn <almero.steyn@gmail.com>
2013-09-24 17:13:31 +0200

    Functional no-impact POC resize directives

--- SingletonTheory.Web.AuthApp/app/js/directives/ResizeDirective.js
+++ SingletonTheory.Web.AuthApp/app/js/directives/ResizeDirective.js
@@ -1,45 +1,33 @@
-﻿userApplicationModule.directive('resizeToWindow', function($window) {
-	return function(scope, element) {
-		var w = angular.element($window);
-		scope.getWindowDimensions = function() {
-			return { 'h': w.height() };
-		};
-		scope.$watch(scope.getWindowDimensions, function(newValue, oldValue) {
-			scope.windowHeight = newValue.h;
-
-			scope.styleToWindow = function() {
-				return {
-					'height': (newValue.h - 200) + 'px'
-				};
-			};
-
-		}, true);
-
-		w.bind('resizeToWindow', function() {
-			scope.$apply();
-		});
-	};
-});
-
-userApplicationModule.directive('resizeToParent', function ($window) {
+﻿userApplicationModule.directive('resizeBody', function ($window) {
 	return function (scope, element) {
 		var w = angular.element($window);
-		var parent = element.parent();
-		scope.getParentHeight = function () {
-			return { 'h': parent.height() };
+		var paddingTop = $('#navCtrl').height() + 'px';
+		var paddingBottom = $('#footerCtrl').height() + 'px';
+		scope.getWindowDimensions = function () {
+			paddingTop = $('#navCtrl').height() + 'px';
+			paddingBottom = $('#footerCtrl').height() + 'px';
+			return { 'h': w.height() };
 		};
-		scope.$watch(scope.getParentHeight, function (newValue, oldValue) {
-
-			scope.styleToParent = function () {
-				return {
-					'height': (newValue.h - 100) + 'px'
-				};
-			};
-
+		scope.$watch(scope.getWindowDimensions, function () {
+			element.css('padding-top', paddingTop);
+			element.css('padding-bottom', paddingBottom);
+			element.height($(window).height() - $('#navCtrl').height() - $('#footerCtrl').height() - 5);
 		}, true);
-
-		w.bind('resizeToParent', function () {
-			scope.$apply();
-		});
 	};
 });
+
+userApplicationModule.directive('resizeToBody', function ($window) {
+	return function (scope, element, attrs) {
+		var bodyHeight = $('body').height();
+		var windowElement = angular.element($window);
+		var pixelAdjustment = parseInt(attrs.resizeToBody);
+		scope.getWindowDimensions = function () {
+			bodyHeight = $('body').height();
+			return { 'h': windowElement.height() };
+		};
+		scope.$watch(scope.getWindowDimensions, function () {
+			element.height(bodyHeight - pixelAdjustment);
+		}, true);
+	};
+});
+
