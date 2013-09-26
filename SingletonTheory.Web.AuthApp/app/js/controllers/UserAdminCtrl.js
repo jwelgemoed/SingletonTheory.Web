a6a37b81cb9856e8b36089b145f1001d75a17d4f
Kolie <kolies@gmail.com>
2013-09-26 13:43:28 +0200

    UserAdminSection

--- SingletonTheory.Web.AuthApp/app/js/controllers/UserAdminCtrl.js
+++ SingletonTheory.Web.AuthApp/app/js/controllers/UserAdminCtrl.js
@@ -15,12 +15,10 @@ userApplicationModule.controller('UserAdminCtrl',
 			role: ['admin', 'user']
 		};
 
-
 		//********** init **********
 		$scope.init = function () {
 			$scope.refresh();
 		};
-
 
 		//---------- properties ----------
 		$scope.regExNoNumbers = /^([^0-9]*)$/;
@@ -35,49 +33,26 @@ userApplicationModule.controller('UserAdminCtrl',
 
 		$scope.editableInPopup = '<button type="button" class="btn btn-default" ng-click="editElement(row)"><i class="icon-edit icon-black"></i></button> ';
 
-		$scope.userGridOptions = {
+		$scope.elementGridOptions = {
 			data: 'users',
 			columnDefs: [{ field: 'UserName', displayName: localize.getLocalizedString('_UserNameHeading_') },
 				{ field: 'Roles[0]', displayName: localize.getLocalizedString('_RoleHeading_') },
 				 { displayName: '', cellTemplate: $scope.editableInPopup, width: 40 }],
 			selectedItems: $scope.selectedElement,
 			multiSelect: false,
+			plugins: [new ngGridFlexibleHeightPlugin()]
 		};
 
-		$scope.saveElement = function () {
-			if ($scope.elementResource.Id == undefined) {
-				if ($scope.elementResource.Active == '')
-					$scope.elementResource.Active = true;
-
-				$scope.elementResource.$add(function () {
-					$scope.toggleCollapse();
-
-				});
-			} else {
-				$scope.elementResource.$update(function () {
-					$scope.toggleCollapse();
-				});
-			}
-		};
-		
 		// User edit area
 
-
-		$scope.cancelElementSave = function () {
-			$scope.toggleCollapse();
-			//setNewResource();
-		};
-
-		$scope.toggleCollapse = function () {
-			$scope.isCollapsed = !$scope.isCollapsed;
-		};
-		
 		$scope.addElement = function () {
 			$scope.isNew = true;
+			$scope.isEdit = false;
 
 			$scope.passwordIsRequired = true;
 
 			$scope.elementResource = new userResource();
+			$scope.elementResource.Id = 0;
 			$scope.elementResource.Active = true;
 			$scope.elementResource.Language = $scope.Language;
 			$scope.elementResource.Roles = [];
@@ -87,13 +62,51 @@ userApplicationModule.controller('UserAdminCtrl',
 		};
 
 		$scope.editElement = function (row) {
-			$scope.elementResource = row.entity;
+			$scope.isNew = false;
 			$scope.isEdit = true;
+			
+		//	$scope.elementResource = new userResource();
+			$scope.elementResource = row.entity;
 			$scope.passwordIsRequired = false;
 			$scope.role = $scope.elementResource.Roles[0];
-			
+
 			$scope.isCollapsed = true;
 		};
+
+		$scope.addNewElement = function () {
+			if ($scope.elementResource.Active == '')
+				$scope.elementResource.Active = true;
+
+			$scope.elementResource.$add(function () {
+				$scope.toggleCollapse();
+				$scope.refresh();
+			},
+			function (error) {
+				$scope.error = error;
+			});
+		};
+
+		$scope.updateElement = function () {
+			
+			$scope.elementResource.$update(function () {
+				$scope.toggleCollapse();
+				$scope.refresh();
+			},
+			function (error) {
+				$scope.error = error;
+			});
+		};
+
+		$scope.cancelElementSave = function () {
+			$scope.toggleCollapse();
+			//setNewResource();
+		};
+
+		$scope.toggleCollapse = function () {
+			$scope.isCollapsed = !$scope.isCollapsed;
+		};
+
+
 
 	}]);
 
