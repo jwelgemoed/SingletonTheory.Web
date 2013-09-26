11008707bcf19b94d724b5395fe2c61ba115781d
AlmeroSteyn <almero.steyn@gmail.com>
2013-09-26 09:06:55 +0200

    Region containers - initial application

--- SingletonTheory.Web.AuthApp/app/js/directives/ResizeDirective.js
+++ SingletonTheory.Web.AuthApp/app/js/directives/ResizeDirective.js
@@ -1,4 +1,5 @@
-﻿
+﻿'use strict';
+
 userApplicationModule.directive('resizeTo', function ($window) {
 	return {
 		link: {
@@ -43,56 +44,3 @@ userApplicationModule.directive('resizeTo', function ($window) {
 	};
 });
 
-/*
-userApplicationModule.directive('resizeToBody', function ($window) {
-	return function (scope, element, attrs) {
-		var bodyHeight = $('body').height();
-		var windowElement = angular.element($window);
-		var pixelAdjustment = parseInt(attrs.resizeToBody);
-		scope.getWindowDimensions = function () {
-			bodyHeight = $('body').height();
-			return { 'h': windowElement.height() };
-		};
-		scope.$watch(scope.getWindowDimensions, function () {
-			element.height(bodyHeight - pixelAdjustment);
-		}, true);
-	};
-});
-*/
-
-//userApplicationModule.directive('resizeBody', function ($window) {
-//	return {
-//		link: {
-//			pre: function preLink(scope, element, attrs) {
-//				var w = angular.element($window);
-//				var paddingTop = $('#navCtrl').height() + 'px';
-//				var paddingBottom = $('#footerCtrl').height() + 'px';
-//				scope.getWindowDimensions = function () {
-//					return { 'h': w.height() };
-//				};
-//				scope.$watch(scope.getWindowDimensions, function () {
-//					paddingTop = $('#navCtrl').height() + 'px';
-//					paddingBottom = $('#footerCtrl').height() + 'px';
-//					element.css('padding-top', paddingTop);
-//					element.css('padding-bottom', paddingBottom);
-//					element.height($(window).height() - $('#navCtrl').height() - $('#footerCtrl').height() - 5);
-//				}, true);
-//			}
-//		}
-//	};
-//	//return function (scope, element) {
-//	//	var w = angular.element($window);
-//	//	var paddingTop = $('#navCtrl').height() + 'px';
-//	//	var paddingBottom = $('#footerCtrl').height() + 'px';
-//	//	scope.getWindowDimensions = function () {
-//	//		return { 'h': w.height() };
-//	//	};
-//	//	scope.$watch(scope.getWindowDimensions, function () {
-//	//		paddingTop = $('#navCtrl').height() + 'px';
-//	//		paddingBottom = $('#footerCtrl').height() + 'px';
-//	//		element.css('padding-top', paddingTop);
-//	//		element.css('padding-bottom', paddingBottom);
-//	//		element.height($(window).height() - $('#navCtrl').height() - $('#footerCtrl').height() - 5);
-//	//	}, true);
-//	//};
-//});
