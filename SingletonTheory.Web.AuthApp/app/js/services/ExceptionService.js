8265f08dd415946ab37f7cf6a4047df16bd46352
JacoVanStaden <code@yakiloo.com>
2013-09-30 10:49:14 +0200

    Fixed $exceptionHandler issue

--- SingletonTheory.Web.AuthApp/app/js/services/ExceptionService.js
+++ SingletonTheory.Web.AuthApp/app/js/services/ExceptionService.js
@@ -1,11 +1,11 @@
 ﻿// Override the default global exception handler.
-angular.module('exceptionOverride', []).factory('$exceptionHandler', function () {
+angular.module('exceptionOverride', []).factory('$exceptionHandler', [function () {
 	return function (exception, cause) {
 		exception.message += ' (caused by "' + cause + '")';
-		$rootScope.exceptionMessage = exception.message;
+		//$rootScope.exceptionMessage = exception.message;
 		//throw exception;
 	};
-});
+}]);
 
 //angular.module('st-exceptions', []).factory('$exceptionHandler', '$rootScope', function ($rootScope) {
 //	return function (exception, cause) {
