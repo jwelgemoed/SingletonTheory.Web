cc152add052bd7c2e91a3563c1e1cf08aed08e8c
Kolie <kolies@gmail.com>
2013-10-02 16:51:35 +0200

    throw taken out

--- SingletonTheory.Web.AuthApp/app/js/services/ExceptionService.js
+++ SingletonTheory.Web.AuthApp/app/js/services/ExceptionService.js
@@ -3,7 +3,7 @@ angular.module('exceptionOverride', []).factory('$exceptionHandler', [function (
 	return function (exception, cause) {
 		exception.message += ' (caused by "' + cause + '")';
 		//$rootScope.exceptionMessage = exception.message;
-		throw exception;
+		//throw exception;
 	};
 }]);
 
