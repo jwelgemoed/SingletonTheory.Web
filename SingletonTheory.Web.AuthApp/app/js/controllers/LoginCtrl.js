5024f506181fdd1966f295f6f35a67d6bad78e8c
JacoVanStaden <code@yakiloo.com>
2013-10-02 14:56:57 +0200

    Changed login controller to use JSON Object for login type

--- SingletonTheory.Web.AuthApp/app/js/controllers/LoginCtrl.js
+++ SingletonTheory.Web.AuthApp/app/js/controllers/LoginCtrl.js
@@ -2,16 +2,17 @@
 
 userApplicationModule.controller('LoginCtrl',
 	['$rootScope', '$scope', '$location', '$window', 'AuthService', function ($rootScope, $scope, $location, $window, authService) {
+		$scope.user = {
+			UserName: '',
+			Password: '',
+			RememberMe: true
+		}
 
 		$scope.login =
 			function () {
 				$scope.RememberMe = $scope.RememberMe == undefined ? false : $scope.RememberMe;
 
-				authService.login({
-					UserName: $scope.UserName,
-					Password: $scope.Password,
-					RememberMe: $scope.RememberMe
-				},
+				authService.login($scope.user,
 				function (res) {
 					$location.path('/');
 				},
