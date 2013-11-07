f0a71b2d703b157e123704ba8d678005d31e96e4
AlmeroSteyn <almero.steyn@gmail.com>
2013-11-07 13:50:30 +0100

    BRID-161 - Basic hours service access

--- SingletonTheory.Web.AuthApp/app/js/controllers/BookedHoursInputCtrl.js
+++ SingletonTheory.Web.AuthApp/app/js/controllers/BookedHoursInputCtrl.js
@@ -1,8 +1,53 @@
 ﻿'use strict';
 
 userApplicationModule.controller('BookedHoursInputCtrl',
-	['$rootScope', '$scope','$timeout', 'localize', 'AuthService',
-		function ($rootScope, $scope,$timeout, localize, AuthService) {
+	['$rootScope', '$scope', '$timeout', 'localize', 'AuthService', 'ItemHoursEntryResource', 'CostCentreResource', 'HourTypeResource',
+		function ($rootScope, $scope, $timeout, localize, AuthService, ItemHoursEntryResource, CostCentreResource, HourTypeResource) {
+
+			$scope.itemHoursResource = [];
+			$scope.costCentres = [];
+			$scope.hourTypes = [];
+			$scope.bookedHourType = "";
+			$scope.productionCostCentre = "";
+			$scope.paintingCostCentre = "";
+
+			$scope.init = function () {
+				$scope.itemHoursResource = new ItemHoursEntryResource();
+				
+				CostCentreResource.query({}, function (result) {
+					$scope.costCentres = result;
+					for (var i = 0; i < result.length; i++) {
+						if (result[i].LookupCode == "PROD" ) {
+							$scope.productionCostCentre = result[i];
+						}
+						if (result[i].LookupCode == "PNTG") {
+							$scope.paintingCostCentre = result[i];
+						}
+					}
+				}, function (err) { $scope.error = err; }
+						);
+				
+				HourTypeResource.query({}, function (result) {
+					$scope.hourTypes = result;
+					for (var i = 0; i < result.length; i++) {
+						if (result[i].LookupCode == "BOOK") {
+							$scope.bookedHourType = result[i];
+							break;
+						}
+					}
+				}, function (err) { $scope.error = err; }
+						);
+			};
+
+			$scope.addHoursEntry = function () {
+				$scope.itemHoursResource.HourType = $scope.bookedHourType;
+				$scope.itemHoursResource.HourTypeId = $scope.bookedHourType.Id;
+				$scope.itemHoursResource.$add({}, function () {
+					alert('success');
+				}, function (err) { $scope.error = err; }
+					);
+			};
+
 			//DatePickers ----------
 
 			$scope.dateOptions = {
