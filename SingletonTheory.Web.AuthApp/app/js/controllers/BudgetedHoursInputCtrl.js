0623f433280245982f96dd14bbd14a6096a7cb5e
AlmeroSteyn <almero.steyn@gmail.com>
2013-11-11 15:01:57 +0100

    BRID-161 Basework for budgeted hours

--- SingletonTheory.Web.AuthApp/app/js/controllers/BudgetedHoursInputCtrl.js
+++ SingletonTheory.Web.AuthApp/app/js/controllers/BudgetedHoursInputCtrl.js
@@ -1,10 +1,10 @@
 ﻿'use strict';
 
-userApplicationModule.controller('BookedHoursInputCtrl',
-	['$rootScope', '$scope', '$timeout', '$exceptionHandler', 'localize', 'AuthService', 'ItemHoursEntryResource', 'CostCentreResource', 'HourTypeResource',
-		function ($rootScope, $scope, $timeout, $exceptionHandler, localize, AuthService, ItemHoursEntryResource, CostCentreResource, HourTypeResource) {
+userApplicationModule.controller('BudgetedHoursInputCtrl',
+	['$rootScope', '$scope', '$timeout', '$exceptionHandler', 'localize', 'AuthService', 'RoomHoursEntryResource', 'CostCentreResource', 'HourTypeResource',
+		function ($rootScope, $scope, $timeout, $exceptionHandler, localize, AuthService, RoomHoursEntryResource, CostCentreResource, HourTypeResource) {
 
-			$scope.itemHoursResource = [];
+			$scope.roomHoursResource = [];
 			$scope.costCentres = [];
 			$scope.hourTypes = [];
 			$scope.bookedHourType = "";
@@ -14,13 +14,13 @@ userApplicationModule.controller('BookedHoursInputCtrl',
 			var previousBlurPerson = "";
 			var previousBlurDate = "";
 			$scope.dateFormat = "dd-MMMM-yyyy";
-			
+
 			setupGrids();
 
 			$scope.$on('localizeResourcesUpdates', function () {
 				setupGrids();
 			});
-			
+
 			function setupGrids() {
 				$scope.hourTypeHeading = localize.getLocalizedString('_HourTypeHeading_');
 				$scope.personNumberHeading = localize.getLocalizedString('_PersonNumberHeading_');
@@ -28,7 +28,6 @@ userApplicationModule.controller('BookedHoursInputCtrl',
 				$scope.costCentreHeading = localize.getLocalizedString('_CostCentreHeading_');
 				$scope.orderNumberHeading = localize.getLocalizedString('_OrderNumberHeading_');
 				$scope.roomNumberHeading = localize.getLocalizedString('_RoomNumberHeading_');
-				$scope.itemNumberHeading = localize.getLocalizedString('_ItemNumberHeading_');
 				$scope.hoursHeading = localize.getLocalizedString('_HoursHeading_');
 				$scope.descriptionHeading = localize.getLocalizedString('_DescriptionHeading_');
 				$scope.hoursEntryGridHeaders = [{ field: 'HourType', displayName: $scope.hourTypeHeading },
@@ -37,11 +36,10 @@ userApplicationModule.controller('BookedHoursInputCtrl',
 					{ field: 'CostCentre', displayName: $scope.costCentreHeading },
 					{ field: 'OrderNumber', displayName: $scope.orderNumberHeading },
 					{ field: 'RoomNumber', displayName: $scope.roomNumberHeading },
-					{ field: 'ItemNumber', displayName: $scope.itemNumberHeading },
 					{ field: 'Hours', displayName: $scope.hoursHeading },
 					{ field: 'Description', displayName: $scope.descriptionHeading }];
 			}
-			
+
 			$scope.enteredHoursGridOptions = {
 				data: 'gridSource',
 				columnDefs: 'hoursEntryGridHeaders',
@@ -50,25 +48,18 @@ userApplicationModule.controller('BookedHoursInputCtrl',
 			};
 
 			$scope.init = function () {
-				$scope.itemHoursResource = new ItemHoursEntryResource();
-				
+				$scope.roomHoursResource = new RoomHoursEntryResource();
+
 				CostCentreResource.query({}, function (result) {
-					for (var i = 0; i < result.length; i++) {
-						if (result[i].LookupCode == "PROD" ) {
-							$scope.costCentres[0] = result[i];
-							$scope.itemHoursResource.CostCentreId = $scope.costCentres[0].Id;
-						}
-						if (result[i].LookupCode == "PNTG") {
-							$scope.costCentres[1] = result[i];
-						}
-					}
+					$scope.costCentres = result;
+					$scope.roomHoursResource.CostCentreId = $scope.costCentres[0].Id;
 				}, function (err) { throw localize.getLocalizedString('_CostCentreError_'); }
 						);
-				
+
 				HourTypeResource.query({}, function (result) {
 					$scope.hourTypes = result;
 					for (var i = 0; i < result.length; i++) {
-						if (result[i].LookupCode == "BOOK") {
+						if (result[i].LookupCode == "PLAN") {
 							$scope.bookedHourType = result[i];
 							break;
 						}
@@ -83,59 +74,57 @@ userApplicationModule.controller('BookedHoursInputCtrl',
 				return new Date(parseInt(dateValue.substr(6))).toString($scope.dateFormat);
 			};
 
-			$scope.personDataChanged = function () {
-				if ($scope.itemHoursResource.PersonNumber != previousBlurPerson || $scope.itemHoursResource.Date != previousBlurDate) {
-					previousBlurPerson = $scope.itemHoursResource.PersonNumber;
-					previousBlurDate = $scope.itemHoursResource.Date;
+			$scope.personDateChanged = function () {
+				if ($scope.roomHoursResource.PersonNumber != previousBlurPerson || $scope.roomHoursResource.Date != previousBlurDate) {
+					previousBlurPerson = $scope.roomHoursResource.PersonNumber;
+					previousBlurDate = $scope.roomHoursResource.Date;
 					resetHourDetails();
 					$scope.gridSource.length = 0;
 				}
 			};
 
 			$scope.addHoursEntry = function () {
-				$scope.itemHoursResource.HourType = $scope.bookedHourType;
-				$scope.itemHoursResource.HourTypeId = $scope.bookedHourType.Id;
-				
-				if ($scope.itemHoursResource.CostCentreId == $scope.costCentres[0].Id) {
-					$scope.itemHoursResource.CostCentre = $scope.costCentres[0];
+				$scope.roomHoursResource.HourType = $scope.bookedHourType;
+				$scope.roomHoursResource.HourTypeId = $scope.bookedHourType.Id;
+
+				if ($scope.roomHoursResource.CostCentreId == $scope.costCentres[0].Id) {
+					$scope.roomHoursResource.CostCentre = $scope.costCentres[0];
 				} else {
-					$scope.itemHoursResource.CostCentre = $scope.costCentres[1];
+					$scope.roomHoursResource.CostCentre = $scope.costCentres[1];
 				}
-				
-				$scope.itemHoursResource.$add({}, function () {
-					
+
+				$scope.roomHoursResource.$add({}, function () {
+
 					var enteredHoursItem = {
-						HourType: $scope.itemHoursResource.HourType.Description,
-						PersonNumber: $scope.itemHoursResource.PersonNumber,
-						Date: $scope.parseJsonDateValue($scope.itemHoursResource.Date),
-						CostCentre: $scope.itemHoursResource.CostCentre.Code + ' - ' + $scope.itemHoursResource.CostCentre.Description,
-						OrderNumber:  $scope.itemHoursResource.OrderNumber,
-						RoomNumber: $scope.itemHoursResource.RoomNumber,
-						ItemNumber: $scope.itemHoursResource.ItemNumber,
-						Hours: $scope.itemHoursResource.Hours,
-						Description: $scope.itemHoursResource.Description
+						HourType: $scope.roomHoursResource.HourType.Description,
+						PersonNumber: $scope.roomHoursResource.PersonNumber,
+						Date: $scope.parseJsonDateValue($scope.roomHoursResource.Date),
+						CostCentre: $scope.roomHoursResource.CostCentre.Code + ' - ' + $scope.roomHoursResource.CostCentre.Description,
+						OrderNumber: $scope.roomHoursResource.OrderNumber,
+						RoomNumber: $scope.roomHoursResource.RoomNumber,
+						Hours: $scope.roomHoursResource.Hours,
+						Description: $scope.roomHoursResource.Description
 					};
 
 					$scope.gridSource[$scope.gridSource.length] = enteredHoursItem;
 					resetHourDetails();
-					$scope.itemHoursResource.CostCentreId = $scope.costCentres[0].Id;
+					$scope.roomHoursResource.CostCentreId = $scope.costCentres[0].Id;
 					document.getElementById('costCentreSelection').focus();
-					
+
 				}, function (err) { throw localize.getLocalizedString('_HourAddError_'); }
 					);
 			};
 
 			function resetHourDetails() {
 				resetDetailFields();
-				$scope.itemHoursResource.Date = previousBlurDate;
+				$scope.roomHoursResource.Date = previousBlurDate;
 			}
 
 			function resetDetailFields() {
-				$scope.itemHoursResource.Id = 0;
-				$scope.itemHoursResource.OrderNumber = "";
-				$scope.itemHoursResource.RoomNumber = "";
-				$scope.itemHoursResource.ItemNumber = "";
-				$scope.itemHoursResource.Hours = "";
-				$scope.itemHoursResource.Description = "";
+				$scope.roomHoursResource.Id = 0;
+				$scope.roomHoursResource.OrderNumber = "";
+				$scope.roomHoursResource.RoomNumber = "";
+				$scope.roomHoursResource.Hours = "";
+				$scope.roomHoursResource.Description = "";
 			}
 		}]);
